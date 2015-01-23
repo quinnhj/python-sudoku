7cd08c9520956c476e7650b6016ad50a3230b15e
Quinn Johnson <quinn@Quinns-MacBook-Pro.local>
2015-01-23 10:58:44 -0800

    Added a slight abstraction around sudoku board impl

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -3,6 +3,23 @@
 import sys
 import copy
 
+class Sudoku:
+    
+    def __init__(self, l):
+        self.board = l
+
+    def get(self, x, y):
+        return self.board[y][x]
+
+    def set(self, x, y, val):
+        self.board[y][x] = val
+    
+    def isFilled(self):
+        for line in self.board:
+            if 0 in line:
+                return False
+        return True
+
 def check_unique(l):
     trimmed = [i for i in l if i != 0]
     return len(trimmed) == len(set(trimmed))
@@ -11,19 +28,19 @@ def check_box(c, xs, ys):
     l = []
     for x in range(xs, xs+3):
         for y in range(ys, ys+3):
-            l.append(c[y][x])
+            l.append(c.get(x,y))
     return check_unique(l)
 
 def check_row(c, y):
     l = []
     for x in range(9):
-        l.append(c[y][x])
+        l.append(c.get(x,y))
     return check_unique(l)
 
 def check_col(c, x):
     l = []
     for y in range(9):
-        l.append(c[y][x])
+        l.append(c.get(x,y))
     return check_unique(l)
 
 def reject(c):
@@ -39,21 +56,19 @@ def reject(c):
 def accept(c):
     if reject(c):
         return False
-    for line in c:
-        if 0 in line:
-            return False
-    return True
+    return c.isFilled()
 
 def extensions(c):
     extension_list = []
-    for line_num, line in enumerate(c):
-        for idx, val in enumerate(line):
+    for x in range(9):
+        for y in range(9):
+            val = c.get(x,y)
             if val == 0:
-                for i in range(1, 10):
+                for i in range(1,10):
                     new_c = copy.deepcopy(c)
-                    new_c[line_num][idx] = i
+                    new_c.set(x,y,i)
                     extension_list.append(new_c)
-                return extension_list 
+                return extension_list
     return extension_list
 
 def backtrack(c):
@@ -73,9 +88,9 @@ if __name__ == "__main__":
     with open(sys.argv[1], 'r') as f:
         for line in f:
             puzzle.append(map(int, list(line)[:-1]))
-    sol = backtrack(puzzle)
+    sol = backtrack(Sudoku(puzzle))
     if sol:
-        for line in sol:
+        for line in sol.board:
             print " ".join(map(str, line))
     else:
         print "Couldn't find a solution"
